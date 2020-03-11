41fa8612b8e3d64776f32ab2128fe6d2e6855384
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-11 17:54:02 +0100

    Add Distribution class

--- datacatalogtordf/distribution.py
+++ datacatalogtordf/distribution.py
@@ -1,4 +1,4 @@
-from rdflib import Namespace
+from rdflib import Namespace, Graph, URIRef, RDF, Literal
 
 DCT = Namespace('http://purl.org/dc/terms/')
 DCAT = Namespace('http://www.w3.org/ns/dcat#')
@@ -11,3 +11,48 @@ class Distribution():
 
     def __init__(self):
         self._type = DCAT.Distribution
+        # set up graph and namespaces:
+        self._g = Graph()
+        self._g.bind('dct', DCT)
+        self._g.bind('dcat', DCAT)
+
+    @property
+    def identifier(self) -> str:
+        return self._identifier
+
+    @identifier.setter
+    def identifier(self, identifier: str):
+        self._identifier = identifier
+
+    @property
+    def title(self) -> dict:
+        return self._title
+
+    @title.setter
+    def title(self, title: dict):
+        self._title = title
+
+    # -
+    def to_rdf(self, format='turtle') -> str:
+        """
+        Maps the distribution to rdf and returns a serialization
+           as a string according to format
+        """
+
+        return self._to_graph().serialize(format=format, encoding='utf-8')
+
+# -
+    def _to_graph(self) -> Graph:
+
+        self._g.add((URIRef(self.identifier), RDF.type, self._type))
+
+        if hasattr(self, 'title'):
+            self._title_to_graph()
+
+        return self._g
+
+    def _title_to_graph(self):
+
+        for key in self._title:
+            self._g.add((URIRef(self.identifier), DCT.title,
+                         Literal(self.title[key], lang=key)))
---
+++ tests/test_distribution.py
@@ -0,0 +1,56 @@
+from datacatalogtordf.distribution import Distribution
+
+from rdflib import Graph
+from rdflib.compare import isomorphic, graph_diff
+# import pytest
+
+
+def test_to_graph_should_return_title_as_graph():
+
+    distribution = Distribution()
+    distribution.identifier = 'http://example.com/distributions/1'
+    distribution.title = {"nb": "API-distribusjon", "en": "API-distribution"}
+
+    src = '''
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+
+    <http://example.com/distributions/1> a dcat:Distribution ;
+        dct:title   "API-distribution"@en, "API-distribusjon"@nb
+        .
+    '''
+    g1 = Graph().parse(data=distribution.to_rdf(), format='turtle')
+    g2 = Graph().parse(data=src, format='turtle')
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+# ---------------------------------------------------------------------- #
+# Utils for displaying debug information
+
+
+def _dump_diff(g1, g2):
+    in_both, in_first, in_second = graph_diff(g1, g2)
+    print("\nin both:")
+    _dump_turtle(in_both)
+    print("\nin first:")
+    _dump_turtle(in_first)
+    print("\nin second:")
+    _dump_turtle(in_second)
+
+
+def _dump_turtle_sorted(g):
+    for l in sorted(g.serialize(format='turtle').splitlines()):
+        if l:
+            print(l.decode())
+
+
+def _dump_turtle(g):
+    for l in g.serialize(format='turtle').splitlines():
+        if l:
+            print(l.decode())
