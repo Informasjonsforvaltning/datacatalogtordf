672beba474fef117227557ce0fe494695fb3ee4f
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-15 13:30:16 +0100

    Add nox as a test automation tool

---
+++ noxfile.py
@@ -0,0 +1,7 @@
+import nox
+
+@nox.session(python=["3.8", "3.7"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
