08a839179dd696272141c2c60ee5313d06af9bca
Stig B. Dørmænen <stigbd@gmail.com>
2020-03-17 13:51:30 +0100

    Single-source the package version

--- src/datacatalogtordf/__init__.py
+++ src/datacatalogtordf/__init__.py
@@ -7,7 +7,16 @@ Modules:
     dataservice
     distribution
 """
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
 from .catalog import Catalog
 from .dataservice import DataService
 from .dataset import Dataset
