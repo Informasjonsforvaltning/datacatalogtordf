25bc44bca8a29abfa6466e0cf4eaf821b72fec26
Stig B. Dørmænen <stigbd@gmail.com>
2020-06-30 16:31:10 +0200

    Add support for media type in dataservice

--- tests/test_dataservice.py
+++ tests/test_dataservice.py
@@ -90,6 +90,35 @@ def test_to_graph_should_return_servesDataset_as_graph() -> None:
     assert _isomorphic
 
 
+def test_to_graph_should_return_media_type() -> None:
+    """It returns a media type graph isomorphic to spec."""
+    dataService = DataService()
+    dataService.identifier = "http://example.com/dataservices/1"
+    dataService.media_types.append(
+        "https://www.iana.org/assignments/media-types/application/ld+json"
+    )
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+
+    <http://example.com/dataservices/1> a dcat:DataService ;
+        dcat:mediaType \
+        <https://www.iana.org/assignments/media-types/application/ld+json> ;
+        .
+    """
+    g1 = Graph().parse(data=dataService.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+
 # ---------------------------------------------------------------------- #
 # Utils for displaying debug information
 
