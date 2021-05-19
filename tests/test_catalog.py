590f0f4ad4da564c9d8c8eed93c27fe3d8e1ee09
Frederik Rønnevig <frederik.code@gmail.com>
2021-05-19 09:48:38 +0200

    feat: #60 adding skolemization to datacatalog class

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,10 +1,13 @@
 """Test cases for the catalog module."""
+
 from typing import Any
 
+from pytest_mock import MockFixture
 from rdflib import Graph, Literal, Namespace, RDF, URIRef
 from rdflib.compare import graph_diff, isomorphic
 
 from datacatalogtordf import Catalog, CatalogRecord, DataService, Dataset
+from tests.testutils import assert_isomorphic, skolemization
 
 DCT = Namespace("http://purl.org/dc/terms/")
 MODELLDCATNO = Namespace("https://data.norge.no/vocabulary/modelldcatno#")
@@ -442,6 +445,31 @@ def test_to_graph_should_return_dct_identifier_as_graph() -> None:
         _dump_diff(g1, g2)
         pass
     assert _isomorphic
+
+
+def test_to_graph_should_return_blank_skolemization(mocker: MockFixture) -> None:
+    """It returns a catalog graph as blank node isomorphic to spec."""
+    catalog = Catalog()
+
+    mocker.patch(
+        "datacatalogtordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a dcat:Catalog  .
+
+        """
+    g1 = Graph().parse(data=catalog.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
 
 
 # ---------------------------------------------------------------------- #
