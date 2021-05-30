af6675e392a70e78804b06ea75cbace4a24299b4
Frederik Rønnevig <frederik.code@gmail.com>
2021-05-30 16:00:43 +0200

    feat: #30 skolemization on dcat:DataService

--- tests/test_dataservice.py
+++ tests/test_dataservice.py
@@ -1,6 +1,8 @@
 """Test cases for the dataservice module."""
+from pytest_mock import MockFixture
 from rdflib import Graph
 from rdflib.compare import graph_diff, isomorphic
+from skolemizer.testutils import skolemization
 
 from datacatalogtordf import DataService, Dataset
 
@@ -18,6 +20,36 @@ def test_to_graph_should_return_identifier_set_at_constructor() -> None:
     <http://example.com/dataservices/1> a dcat:DataService ;
         .
     """
+    g1 = Graph().parse(data=dataService.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+
+def test_to_graph_should_return_skolemization(mocker: MockFixture) -> None:
+    """It returns a endpointURL graph isomorphic to spec."""
+    dataService = DataService()
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+
+    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+        a dcat:DataService ;
+        .
+    """
+
+    mocker.patch(
+        "skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
+
     g1 = Graph().parse(data=dataService.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
