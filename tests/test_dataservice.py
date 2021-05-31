cd6deae69b9d9b0ec0630a6a0714e72bf9748be9
Frederik Rønnevig <frederik.code@gmail.com>
2021-05-31 06:52:32 +0200

    feat: #30 skolemization on dcat:servesDataset on dcat:DataService

--- tests/test_dataservice.py
+++ tests/test_dataservice.py
@@ -145,6 +145,43 @@ def test_to_graph_should_return_servesDataset_as_graph() -> None:
     assert _isomorphic
 
 
+def test_to_graph_should_return_servesDataset_skolemization(
+    mocker: MockFixture,
+) -> None:
+    """It returns a servesDataset graph isomorphic to spec."""
+    dataService = DataService()
+    dataService.identifier = "http://example.com/dataservices/1"
+
+    dataset1 = Dataset()
+    dataService.servesdatasets.append(dataset1)
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+
+    <http://example.com/dataservices/1> a dcat:DataService ;
+    dcat:servesDataset
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+    .
+    """
+
+    mocker.patch(
+        "skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
+
+    g1 = Graph().parse(data=dataService.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+
 def test_to_graph_should_return_media_type() -> None:
     """It returns a media type graph isomorphic to spec."""
     dataService = DataService()
